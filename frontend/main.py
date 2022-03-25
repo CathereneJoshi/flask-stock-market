92a4d8d21b8bb03f9fbae8e051f33f4ca7537b2a
CathereneJoshi <catherenejoshi@gmail.com>
2022-03-25 15:55:05 +0530

    Added equity news section

--- frontend/main.py
+++ frontend/main.py
@@ -1,8 +1,14 @@
 from flask import Flask, render_template
+from EquityBulls import limited
+
 app = Flask(__name__) #define app
 
 @app.route('/') #path set
 def home():
     return render_template('index.html')
+
+@app.route('/news') #path set
+def news():
+    return render_template('news.html',limited=limited)
 
 app.run(debug=True)
